a97ea141cca0ecf0915c1369fc8fbe7886c79bea
Evelyn Sena <78700147+EvelynSn@users.noreply.github.com>
2024-03-29 21:46:13 -0300

    Add files via upload

---
+++ bee1837.py
@@ -0,0 +1,4 @@
+#CONVERSOR DE TEMPERATURA SIMPLES
+n1=float(input('Quantos graus está? \n'));
+print('Em FAHRENHEIT TEMOS {} °F' .format((n1*1.8)+32));
+print("Em KELVIN temos {:.2f} K" .format(n1+273.15));
